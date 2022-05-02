5e6078868c48a691005932dd04b9a0c2ed6f0f90
Han Xiao <1043599451@qq.com>
2022-05-02 18:54:49 +0800

    javaConfig

--- java_config/src/main/java/com/hanxiao/config/SpringConfig.java
+++ java_config/src/main/java/com/hanxiao/config/SpringConfig.java
@@ -1,7 +1,13 @@
 package com.hanxiao.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
+import com.alibaba.druid.pool.DruidDataSource;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.*;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 /**
  * @description:
@@ -9,7 +15,32 @@ import org.springframework.stereotype.Component;
  * @date: 2022/5/2
  **/
 @Configuration
-@Component
+@ComponentScan("com.hanxiao")
+@PropertySource("classpath:param.properties")
+@EnableAspectJAutoProxy
+@EnableTransactionManagement
 public class SpringConfig {
 
+    @Bean
+    public DataSource dataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/spring_db?" +
+                "useSSL=false&useUnicode=true&characterEncoding=utf-8");
+        dataSource.setUsername("root");
+        dataSource.setPassword("123456");
+        return dataSource;
+    }
+
+    @Bean
+    public SqlSessionFactoryBean sqlSessionFactoryBean(DataSource dataSource) {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        return sqlSessionFactoryBean;
+    }
+
+    @Bean
+    public DataSourceTransactionManager transactionManager(@Qualifier("dataSource") DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
 }
