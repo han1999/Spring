a54f77c040ebdb91c303d573964f65bd5c510665
Han Xiao <1043599451@qq.com>
2022-05-04 23:28:50 +0800

    method, params in RequestMapping

--- spring_mvc/src/main/java/com/hanxiao/controller/HelloController.java
+++ spring_mvc/src/main/java/com/hanxiao/controller/HelloController.java
@@ -1,9 +1,9 @@
 package com.hanxiao.controller;
 
+import com.hanxiao.bean.BaseRespVO;
 import com.hanxiao.bean.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -25,5 +25,66 @@ public class HelloController {
     @ResponseBody
     public User json() {
         return new User("hanxiao", "123456");
+    }
+
+    @RequestMapping({"hello1", "hello2", "hello3"})
+    @ResponseBody
+    public BaseRespVO hello() {
+        return BaseRespVO.ok("hello");
+    }
+
+
+    @RequestMapping("goodbye*")
+    @ResponseBody
+    public BaseRespVO goodbye1() {
+        return BaseRespVO.ok("goodbye*");
+    }
+
+    @RequestMapping("goodbye/*")
+    @ResponseBody
+    public BaseRespVO goodbye2() {
+        return BaseRespVO.ok("goodbye/*");
+    }
+
+    @RequestMapping(value = "method/get", method = RequestMethod.GET)
+    @ResponseBody
+    public BaseRespVO methodGet() {
+        return BaseRespVO.ok("get");
+    }
+
+    @RequestMapping(value = "method/post", method = RequestMethod.POST)
+    @ResponseBody
+    public BaseRespVO methodPost() {
+        return BaseRespVO.ok("post");
+    }
+
+    @GetMapping("method/get1")
+    @ResponseBody
+    public BaseRespVO methodGet1() {
+        return BaseRespVO.ok("get1");
+    }
+
+    @PostMapping("method/post1")
+    @ResponseBody
+    public BaseRespVO methodPost1() {
+        return BaseRespVO.ok("post1");
+    }
+
+    @RequestMapping(value = "method/double", method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public BaseRespVO methodDouble() {
+        return BaseRespVO.ok("double");
+    }
+
+    @RequestMapping(value = "login", params = "username")
+    @ResponseBody
+    public BaseRespVO login() {
+        return BaseRespVO.ok("login");
+    }
+
+    @RequestMapping(value = "login2", params = {"username", "password"})
+    @ResponseBody
+    public BaseRespVO login2() {
+        return BaseRespVO.ok("login2");
     }
 }
