d80a5f94c6c57e07ca6b17bc2317bb23761eef86
Han Xiao <1043599451@qq.com>
2022-05-09 21:56:06 +0800

    ReloadableResourceBundleMessageSource

--- spring_mvc4/src/main/java/com/hanxiao/controller/HelloController.java
+++ spring_mvc4/src/main/java/com/hanxiao/controller/HelloController.java
@@ -1,6 +1,10 @@
 package com.hanxiao.controller;
 
 import com.hanxiao.bean.BaseRespVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.MessageSource;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +20,15 @@ public class HelloController {
     @RequestMapping("hello")
     public BaseRespVO hello(Locale locale) {
         return BaseRespVO.ok(locale);
+    }
+
+    @Autowired
+    @Qualifier("messageSource")
+    MessageSource messageSource;
+
+    @RequestMapping("hello1/{key}")
+    public BaseRespVO hello1(Locale locale, @PathVariable String key, String[] strings) {
+        String message = messageSource.getMessage(key, strings, locale);
+        return BaseRespVO.ok(message);
     }
 }
