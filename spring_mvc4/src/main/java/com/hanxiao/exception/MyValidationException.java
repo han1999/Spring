5e59aa6b285df196ce021b484d98bba738f8c819
Han Xiao <1043599451@qq.com>
2022-05-10 11:40:39 +0800

    BindingResult

---
+++ spring_mvc4/src/main/java/com/hanxiao/exception/MyValidationException.java
@@ -0,0 +1,14 @@
+package com.hanxiao.exception;
+
+/**
+ * @description:
+ * @author: Han Xiao
+ * @date: 2022/5/10
+ **/
+
+public class MyValidationException extends Exception {
+
+    public MyValidationException(String message) {
+        super(message);
+    }
+}
